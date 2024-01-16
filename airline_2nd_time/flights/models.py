7d15bb9fa1d7f368b6a570e857458eaf72ecfdfe
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-16 07:42:34 -0800

    out no where i dont know whats happening

--- airline_2nd_time/flights/models.py
+++ airline_2nd_time/flights/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
 class Airport(models.Model):
   code = models.CharField(max_length=3)
-  city = models.CharField(max_length=64)
+  city = models.CharField(max_length=64) 
 
   def __str__(self):
-    return f"{self.city} ({self.code})"
-  
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
   origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
-  destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arriavals")
+  destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
   duration = models.IntegerField() 
 
   def __str__(self):
-    return f"{self.id}: {self.origin} to {self.destination}"
+    return f"{self.origin} to {self.destination}"

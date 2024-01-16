ba48929e0125269f561cd119a885103e25f1cfcd
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-16 10:51:22 -0800

    the book func is done but i havent created the form yet

--- airline_3rd_time/flights/urls.py
+++ airline_3rd_time/flights/urls.py
@@ -1,7 +1,10 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 
 urlpatterns = [ 
-  
+  path("", views.index, name="index"),
+  path("<int:flight_id>", views.flight, name="flight"),
+  path("<int:flight_id>/book", views.book, name="book")
+
 ]
--- airline_3rd_time/flights/views.py
+++ airline_3rd_time/flights/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Flight, Passengers
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+def index(request):
+  return render(request, "flights/index.html", {
+    "flights": Flight.objects.all()
+  })
+
+def flight(request,flight_id):
+  flight = Flight.objects.get(pk=flight_id)
+  return render(request, "flights/flight.html",{
+    "flight": flight,
+    "passengers": flight.passengers.all()
+
+  })
+
+def book(request,flight_id):
+  if request.method == "POST":
+    flight = Flight.objects.get(pk=flight_id)
+    passenger = Passengers.objects.get(pk=int(request.POST['passenger']))
+    passenger.flights.add(flight_id)
+    return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
